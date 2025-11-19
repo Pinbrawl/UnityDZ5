bf30d5a9d49995583718ea9e72cd762f7d223ad2
Pinbrawl <sans-andertale@mail.ru>
2025-11-19 07:22:46 +0400

    Visual update 19.11.25 1

--- Assets/Scripts/Unit/UnitAnimator.cs
+++ Assets/Scripts/Unit/UnitAnimator.cs
@@ -1,9 +1,13 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
 public class UnitAnimator : MonoBehaviour
 {
     [SerializeField] private Unit _unit;
+    [SerializeField] private string _emotion1;
+    [SerializeField] private int _emotionsChance;
+    [SerializeField] private float _interval;
 
     private static readonly int s_IsRun = Animator.StringToHash("IsRun");
 
@@ -12,6 +16,8 @@ public class UnitAnimator : MonoBehaviour
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+
+        StartCoroutine(CheckChance());
     }
 
     private void OnEnable()
@@ -27,5 +33,19 @@ public class UnitAnimator : MonoBehaviour
     private void SetRunParameter(bool isRun)
     {
         _animator.SetBool(s_IsRun, isRun);
+    }
+
+    private IEnumerator CheckChance()
+    {
+        var waitTime = new WaitForSecondsRealtime(_interval);
+
+        while (enabled)
+        {
+            yield return waitTime;
+
+            if (_animator.GetBool(s_IsRun) == false)
+                if (Random.Range(0, 100) < _emotionsChance)
+                    _animator.Play(_emotion1);
+        }
     }
 }
